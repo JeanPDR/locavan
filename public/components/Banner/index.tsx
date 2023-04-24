37ce30e07a89850deea06169fcf580693735bb0b
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2023-04-24 19:23:15 -0300

    UPDATE - Finalizando Banner

--- public/components/Banner/index.tsx
+++ public/components/Banner/index.tsx
@@ -2,16 +2,26 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './styles.module.css';
 
 import 'swiper/css';
+import { Autoplay } from 'swiper';
+
 
 export const Banner = () => {
     return(
         <div className={styles.container}>
             <Swiper
                 slidesPerView={1}
+                loop={true}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                className={styles.swiper}
+
                 >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide className={styles.slide}><img src='/tmp/banner1.png' alt=''></img></SwiperSlide>
+                <SwiperSlide className={styles.slide}><img src='/tmp/banner2.png' alt=''></img></SwiperSlide>
+                
             </Swiper>
         </div>
     )
