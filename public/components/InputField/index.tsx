5d680793b8a2dd334f07314310cd986a7c6aa75e
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2023-04-27 19:02:18 -0300

    UPDATE - Finalizando a criação dos inputs

--- public/components/InputField/index.tsx
+++ public/components/InputField/index.tsx
@@ -1,6 +1,6 @@
 import styles from './styles.module.css';
-import EyeOff from './EyeOff.svg';
-import EyeOn from './EyeOn.svg';
+import Happy from './Happy.svg';
+import Smile from './Smile.svg';
 import  {useState}  from 'react';
 
 type Props = {
@@ -13,20 +13,33 @@ type Props = {
 }
 
 export const InputField = ({color, placeholder, value, onChange, password}: Props) => {
+    const [focused, setShowFocused] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
+
+   
     return(
-        <div className={styles.container}>
+        <div className={styles.container}
+        style={{ 
+            borderColor : focused ? color : '#f9f9fb',
+            backgroundColor : focused ? '#FFF': '#f9f9fb'
+        }}
+        >
             <input
-                type= {password ? 'password' : 'text'}
+                type= {password ?(showPassword ? 'text' : 'password') : 'text'}
                 className={styles.input}
                 placeholder={placeholder}
                 value={value}
                 onChange={e => onChange(e.target.value)}
+                onFocus={() => setShowFocused(true)}
+                onBlur={() => setShowFocused(false)}
             />
             {password &&
-            <div className={styles.showPassword}>
-                {showPassword && <EyeOn color='#e91e63'/>}
-                {!showPassword && <EyeOff color='#e91e63'/>}
+            <div
+            className={styles.showPassword}
+            onClick={() => setShowPassword(!showPassword)}
+            >
+                {showPassword && <Smile color='#e91e63'/>}
+                {!showPassword && <Happy color='#e91e63'/>}
             </div>
             }
         </div>
